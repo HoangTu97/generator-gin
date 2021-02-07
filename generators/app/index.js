6fa105aff450fb7efbcf57cdc706b1557defcbfd
HoangTuNX <tu.nhx@big-labs.com>
2021-02-07 23:00:10 +0700

    add app key

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,33 +1,39 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Welcome to the fine ${chalk.red('generator-gin')} generator!`)
+      yosay(`Welcome to the fine ${chalk.red("generator-gin")} generator!`)
     );
 
     const prompts = [
       {
-        type: 'input',
-        name: 'appName',
-        message: 'Your project name?',
+        type: "input",
+        name: "appName",
+        message: "Your project name?",
         default: this.config.get("appName") || this.appname
       },
       {
-        type: 'input',
-        name: 'appSecretKey',
-        message: 'Your JWT secret key?',
-        default: this.config.get("jwtSecretKey") || Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+        type: "input",
+        name: "jwtSecretKey",
+        message: "Your JWT secret key?",
+        default: this.config.get("jwtSecretKey") || this._jwtGenKey()
       },
       {
-        type: 'input',
-        name: 'serverPort',
-        message: 'Your server port?',
-        default: this.config.get("serverPort") || '8080'
+        type: "input",
+        name: "serverPort",
+        message: "Your server port?",
+        default: this.config.get("serverPort") || "8080"
+      },
+      {
+        type: "input",
+        name: "appSecretKey",
+        message: "Your App secret key?",
+        default: this.config.get("appSecretKey") || this._jwtGenKey()
       }
     ];
 
@@ -36,6 +42,49 @@ module.exports = class extends Generator {
       this.props.destinationPath = this.destinationPath();
     });
   }
+
+  _jwtGenKey() {
+    return (
+      Math.random()
+        .toString(36)
+        .substring(2, 15) +
+      Math.random()
+        .toString(36)
+        .substring(2, 15)
+    );
+  }
+
+  // _appGenKey() {
+  //   var i,
+  //     j,
+  //     k = "";
+
+  //   addEntropyTime();
+  //   var seed = keyFromEntropy();
+
+  //   var prng = new AESprng(seed);
+  //   if (document.key.keytype[0].checked) {
+  //     // Text key
+  //     var charA = "A".charCodeAt(0);
+
+  //     for (i = 0; i < 12; i++) {
+  //       if (i > 0) {
+  //         k += "-";
+  //       }
+  //       for (j = 0; j < 5; j++) {
+  //         k += String.fromCharCode(charA + prng.nextInt(25));
+  //       }
+  //     }
+  //   } else {
+  //     // Hexadecimal key
+  //     var hexDigits = "0123456789ABCDEF";
+
+  //     for (i = 0; i < 64; i++) {
+  //       k += hexDigits.charAt(prng.nextInt(15));
+  //     }
+  //   }
+  //   return k;
+  // }
 
   writing() {
     this.fs.copyTpl(
@@ -46,17 +95,16 @@ module.exports = class extends Generator {
   }
 
   install() {
-    this.spawnCommand('make', ['docs']);
-    this.spawnCommand('go', ['mod','vendor']);
+    this.spawnCommand("make", ["docs"]);
+    this.spawnCommand("go", ["mod", "vendor"]);
   }
 
   end() {
-    this.log(
-      yosay(`Saving config!`)
-    );
-    this.config.set("appName", this.props.appName)
-    this.config.set("jwtSecretKey", this.props.appSecretKey)
-    this.config.set("serverPort", this.props.serverPort)
+    this.log(yosay(`Saving config!`));
+    this.config.set("appName", this.props.appName);
+    this.config.set("appSecretKey", this.props.appSecretKey);
+    this.config.set("jwtSecretKey", this.props.jwtSecretKey);
+    this.config.set("serverPort", this.props.serverPort);
     this.config.save();
   }
 };
